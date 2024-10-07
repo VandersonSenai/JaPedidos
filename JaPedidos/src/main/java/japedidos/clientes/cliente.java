3a24ca9e8d1f51b56f0027909eef3936fe59c3ca
FranqFrak <franquilei.oliveira@aluno.senai.br>
2024-10-07 18:56:06 -0300

    atualiza tela de cliente

--- JaPedidos/src/main/java/japedidos/clientes/cliente.java
+++ JaPedidos/src/main/java/japedidos/clientes/cliente.java
@@ -26,76 +26,105 @@ public class cliente extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jFrame1 = new javax.swing.JFrame();
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-        jlbl_clientes = new javax.swing.JLabel();
-        jlbl_clientes1 = new javax.swing.JLabel();
-        jlbl_clientes2 = new javax.swing.JLabel();
-        jlbl_clientes3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel1.setForeground(new java.awt.Color(102, 255, 255));
+        jLabel2.setIcon(new javax.swing.ImageIcon("C:\\Users\\f.oliveira\\Pictures\\Redimen\\painel_comandos_esquerda.png")); // NOI18N
+        jLabel2.setText("jLabel2");
 
-        jlbl_clientes.setBackground(new java.awt.Color(153, 204, 255));
-        jlbl_clientes.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
-        jlbl_clientes.setText("produtos");
+        jLabel5.setText("clientes");
 
-        jlbl_clientes1.setBackground(new java.awt.Color(153, 204, 255));
-        jlbl_clientes1.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
-        jlbl_clientes1.setText("pedidos");
-
-        jlbl_clientes2.setBackground(new java.awt.Color(153, 204, 255));
-        jlbl_clientes2.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
-        jlbl_clientes2.setText("relatórios");
-
-        jlbl_clientes3.setBackground(new java.awt.Color(153, 204, 255));
-        jlbl_clientes3.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
-        jlbl_clientes3.setText("CLIENTES");
+        jLabel6.setText("jLabel6");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jlbl_clientes2)
-                    .addComponent(jlbl_clientes)
-                    .addComponent(jlbl_clientes3)
-                    .addComponent(jlbl_clientes1))
-                .addContainerGap(656, Short.MAX_VALUE))
+            .addGap(0, 100, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(101, 101, 101)
-                .addComponent(jlbl_clientes3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jlbl_clientes)
-                .addGap(3, 3, 3)
-                .addComponent(jlbl_clientes1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jlbl_clientes2)
-                .addContainerGap(288, Short.MAX_VALUE))
+            .addGap(0, 100, Short.MAX_VALUE)
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        jLabel4.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel4.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
+        jLabel4.setText("PRODUTOS");
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(930, 320, 100, 30);
+
+        jLabel8.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel8.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
+        jLabel8.setText("PEDIDOS");
+        getContentPane().add(jLabel8);
+        jLabel8.setBounds(900, 360, 100, 30);
+
+        jLabel14.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel14.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
+        jLabel14.setText("RELATÓRIOS");
+        getContentPane().add(jLabel14);
+        jLabel14.setBounds(950, 280, 100, 40);
+
+        jLabel18.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel18.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
+        jLabel18.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel18.setText("CLIENTES");
+        getContentPane().add(jLabel18);
+        jLabel18.setBounds(910, 200, 100, 30);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 220, Short.MAX_VALUE)
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 440, Short.MAX_VALUE)
         );
+
+        getContentPane().add(jPanel2);
+        jPanel2.setBounds(1150, 10, 220, 440);
+
+        jButton1.setText("CLIENTES");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(900, 130, 90, 23);
+
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\f.oliveira\\Pictures\\Redimen\\background_clientes.png")); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 0, 830, 450);
+
+        jLabel3.setBackground(new java.awt.Color(0, 102, 255));
+        jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\f.oliveira\\Pictures\\Redimen\\painel_comandos_esquerda.png")); // NOI18N
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(1000, 20, 153, 288);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -136,10 +165,19 @@ public class cliente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JFrame jFrame1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel18;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel jlbl_clientes;
-    private javax.swing.JLabel jlbl_clientes1;
-    private javax.swing.JLabel jlbl_clientes2;
-    private javax.swing.JLabel jlbl_clientes3;
+    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
