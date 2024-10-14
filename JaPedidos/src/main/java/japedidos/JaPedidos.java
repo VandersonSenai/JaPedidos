97ec1d93172d2320e930a84c281d52f739be4d7d
tbaiense <thiago.baiense@aluno.senai.br>
2024-10-14 18:56:45 -0300

    transforma InfoAdicionalCliente em inner class de Cliente em japedidos.clientes.Cliente

--- JaPedidos/src/main/java/japedidos/JaPedidos.java
+++ JaPedidos/src/main/java/japedidos/JaPedidos.java
@@ -2,12 +2,16 @@ package japedidos;
 
 import japedidos.bd.BD;
 import japedidos.produto.*;
+import japedidos.clientes.*;
 import javax.swing.JOptionPane;
 import java.util.Arrays;
 
 public class JaPedidos {
 
     public static void main(String[] args) {
-        
+        Cliente eu = new Cliente("thiago", "12313244");
+        Cliente.InfoAdicional.PF info = new Cliente.InfoAdicional.PF(eu.nome, "928.814.780-26");
+        eu.setInfoAdicional(info);
+        System.out.print(eu.getInfoAdicional());
     }
 }
--- JaPedidos/src/main/java/japedidos/pedidos/Pedido.java
+++ JaPedidos/src/main/java/japedidos/pedidos/Pedido.java
@@ -1,9 +1,12 @@
 package japedidos.pedidos;
 
 import java.util.ArrayList;
+import japedidos.clientes.Cliente;
 import japedidos.produto.Categoria;
 import japedidos.produto.Unidade;
 import japedidos.produto.Produto;
+import japedidos.usuario.Usuario;
+import java.time.LocalDateTime;
 
 /**
  *
@@ -12,5 +15,13 @@ import japedidos.produto.Produto;
 public class Pedido {
     private int id = -1;
     public final ArrayList<EstadoPedido> ESTADOS = new ArrayList<EstadoPedido>();
+    private Destino destino;
+    private String destinatario;
+    public final Usuario AUTOR;
+    private LocalDateTime CRIACAO;
+    private Cliente cliente;
     
+    public Pedido() {
+        this.AUTOR = Usuario.getAtual();
+    }
 }
