02db02a6985b6b7390d07f78b39aadd06b871304
tbaiense <thiago.baiense@aluno.senai.br>
2024-10-15 11:28:22 -0300

    cria tabela produto no script do BD e adiciona coluna "estado" na tabela produto e no modelo físico, em conformidade com os requisitos funcionais

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -37,5 +37,28 @@ INSERT INTO est_andamento(nome) VALUES
 
 SELECT * FROM est_andamento;
 
+CREATE TABLE produto(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id_categoria INT NOT NULL,
+	FOREIGN KEY (id_categoria) REFERENCES categoria(id),
+	id_unidade INT NOT NULL,
+	FOREIGN KEY (id_unidade) REFERENCES unidade(id),
+	nome VARCHAR(32) NOT NULL,
+	preco_venda DECIMAL(8,2) NOT NULL DEFAULT 0,
+	preco_custo DECIMAL(8,2) NOT NULL DEFAULT 0,
+	estado BIT(1) NOT NULL DEFAULT b'1',
+	id_usuario_alt INT,
+	dthr_alt DATETIME
+);
+
+INSERT INTO 
+	produto(id_categoria, id_unidade, nome, preco_venda)
+VALUE
+	(52, 13, "Primeiro produto :D", 25.00);
+
+SELECT produto.nome, categoria.nome, unidade.abreviacao, produto.preco_venda, produto.estado
+FROM produto 
+	INNER JOIN unidade ON unidade.id=produto.id_unidade 
+	INNER JOIN categoria ON categoria.id = produto.id_categoria;
 
 
