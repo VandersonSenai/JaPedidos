358e3658a3e69b29544e164560fdbe6c061caa40
tbaiense <thiago.baiense@aluno.senai.br>
2024-10-14 23:05:09 -0300

    detalha variáveis de japedidos.pedidos.Pedido, corrige cardinalidade da tabela destino no modelo do BD, adiciona coluna id_intercorrencia à tabela intercorrencia e altera sua cardinalidade no relacionamento com pedido

--- JaPedidos/src/main/java/japedidos/pedidos/Pedido.java
+++ JaPedidos/src/main/java/japedidos/pedidos/Pedido.java
@@ -7,6 +7,7 @@ import japedidos.produto.Unidade;
 import japedidos.produto.Produto;
 import japedidos.usuario.Usuario;
 import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 /**
  *
@@ -15,11 +16,23 @@ import java.time.LocalDateTime;
 public class Pedido {
     private int id = -1;
     public final ArrayList<EstadoPedido> ESTADOS = new ArrayList<EstadoPedido>();
+    private TipoEntrega tipoEntrega;
     private Destino destino;
     private String destinatario;
     public final Usuario AUTOR;
-    private LocalDateTime CRIACAO;
+    private LocalDateTime criacao;
+    private Usuario usuarioAlteracao;
+    private LocalDateTime dthrAlteracao;
+    private LocalDateTime dthrAntregar;
+    private LocalDate dtPago;
+    private LocalDate dtVencimentoPagamento;
     private Cliente cliente;
+    private boolean cancelado;
+    private String infoCancelamento;
+    private double precoFrete;
+    private double precoFinal;
+    private double precoCustoTotal;
+    private double taxaDesconto;
     
     public Pedido() {
         this.AUTOR = Usuario.getAtual();
