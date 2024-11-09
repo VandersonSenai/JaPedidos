3c776e4a077dd673b95b81b052bfe4249bebaeff
tbaiense <thiago.baiense@aluno.senai.br>
2024-11-09 13:29:59 -0300

    corrige sql de adicionar itens ao bd

--- db/add_categoria_unidade_produto_usuario.sql
+++ db/add_categoria_unidade_produto_usuario.sql
@@ -1,3 +1,13 @@
+INSERT INTO usuario( nome, login, senha, tipo )
+VALUES
+	("Valentin Gomes"			, "valentin"	, "admin", "administrador"),
+	("Vanderson da Silva"	, "vanderson"	,	"silva", "administrador"),
+	("Thiago M."					, "thiago"		,	"admin", "administrador");
+
+INSERT INTO cliente (nome, telefone) VALUES (
+	"Josefina Andrade", "40028922"
+);
+
 INSERT INTO `categoria` VALUES
 	(1,'Bebida','Produtos como refrigerantes, água, sucos.'),
 	(2,'Lanche','Produtos como hambúrger e similares.'),
@@ -26,35 +36,27 @@ INSERT INTO `unidade` VALUES
 	(12,'Pote','POTE');
        
 INSERT INTO produto( id_categoria, id_unidade, nome, preco_venda, preco_custo, id_usuario_alt )
-VALUES	(5	, 8		,	"SORVETE LUIGI 1,5lt"		, 19.50	, 12.45	,	2),
-		(5	, 8		,	"SORVETE LUIGI 1,7lt"		, 21.75	, 14.15	, 	2),
-        (5	, 8		,	"AÇAI 1,2lt"				, 18.75	, 13.15	, 	2),
-		(1	, 11	,	"COCA-COLA 2lts"			, 14.00	, 9.00	,	1),
-		(1	, 11	,	"COCA-COLA 600ml"			, 9.00	, 6.50	,	1),
-        (1	, 11	,	"GUARANA ANTARTICA 2lts	"	, 14.00	, 9.00	, 	1),
-        (1	, 11	,	"GUARANA ANTARTICA 600ml"	, 9.00	, 6.50	, 	1),
-        (1	, 11	,	"HEINIKEN 600lm"			, 16.50	, 10.50	, 	1),
-        (1	, 11	,	"FANTA UVA 2lT"				, 14.00	, 9.50	, 	1),
-		(4	, 1		,	"COXINHA DE FRANGO"			, 0.75	, 0.15	,	1),
-		(4	, 1		,	"RIZOLE DE PIZZA"			, 0.75	, 0.15	, 	1),
-        (4	, 1		,	"BOLINHA DE QUEIJO"			, 0.75	, 0.15	, 	1),
-        (4	, 1		,	"RISSOLE DE CAMARÃO"		, 0.75	, 0.15	, 	1),
-		(5	, 1		,	"COXINHA DE FRANGO"			, 0.75	, 0.15	,	1),
-		(5	, 1		,	"RIZOLE DE PIZZA"			, 0.75	, 0.15	, 	1),
-        (5	, 1		,	"BOLINHA DE QUEIJO"			, 0.75	, 0.15	, 	1),
-        (5	, 1		,	"KIBE DE CARNE"				, 0.75	, 0.15	, 	1),
-        (5	, 1		,	"RISSOLE DE CAMARÃO"		, 0.75	, 0.15	, 	1),
-        (5	, 1		,	"MINI CHURROS"				, 0.75	, 0.15	, 	1),
-        (5	, 1		,	"ENROLADO DE SALSICHA "		, 0.75	, 0.15	, 	1);
+VALUES	(5	, 8		,	"SORVETE LUIGI 1,5lt"			, 19.50	, 12.45	,	2),
+				(5	, 8		,	"SORVETE LUIGI 1,7lt"			, 21.75	, 14.15	, 2),
+        (5	, 8		,	"AÇAI 1,2lt"							, 18.75	, 13.15	, 2),
+				(1	, 11	,	"COCA-COLA 2lts"					, 14.00	, 9.00	,	1),
+				(1	, 11	,	"COCA-COLA 600ml"					, 9.00	, 6.50	,	1),
+        (1	, 11	,	"GUARANA ANTARTICA 2lts	"	, 14.00	, 9.00	, 1),
+        (1	, 11	,	"GUARANA ANTARTICA 600ml"	, 9.00	, 6.50	, 1),
+        (1	, 11	,	"HEINIKEN 600lm"					, 16.50	, 10.50	, 1),
+        (1	, 11	,	"FANTA UVA 2lT"						, 14.00	, 9.50	, 1),
+				(4	, 1		,	"COXINHA DE FRANGO"				, 0.75	, 0.15	,	1),
+				(4	, 1		,	"RIZOLE DE PIZZA"					, 0.75	, 0.15	, 1),
+        (4	, 1		,	"BOLINHA DE QUEIJO"				, 0.75	, 0.15	, 1),
+        (4	, 1		,	"RISSOLE DE CAMARÃO"			, 0.75	, 0.15	,	1),
+				(5	, 1		,	"COXINHA DE FRANGO"				, 0.75	, 0.15	,	1),
+				(5	, 1		,	"RIZOLE DE PIZZA"					, 0.75	, 0.15	,	1),
+        (5	, 1		,	"BOLINHA DE QUEIJO"				, 0.75	, 0.15	,	1),
+        (5	, 1		,	"KIBE DE CARNE"						, 0.75	, 0.15	,	1),
+        (5	, 1		,	"RISSOLE DE CAMARÃO"			, 0.75	, 0.15	,	1),
+        (5	, 1		,	"MINI CHURROS"						, 0.75	, 0.15	,	1),
+        (5	, 1		,	"ENROLADO DE SALSICHA "		, 0.75	, 0.15	, 1);
         
-INSERT INTO usuario( nome, login, senha, tipo )
-VALUES	("Vanderson da Silva"	, "vanderson"	,	"silva", "administrador"),
-		("Thiago M."	, "thiago"	,	"admin", "administrador")
-;
-
-INSERT INTO cliente (nome, telefone) VALUES (
-	"Josefina Andrade", "40028922"
-);
 
 SELECT p.id, p.nome, c.nome as categoria, p.preco_venda , p.preco_custo, u.abreviacao as unidade, p.estado
 FROM produto as p
