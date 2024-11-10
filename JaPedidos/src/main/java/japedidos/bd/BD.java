0645f6786371f28bfe9ebe760ab96a3f2cfe88c5
tbaiense <thiago.baiense@aluno.senai.br>
2024-11-10 16:24:56 -0300

    altera info de conexão ao banco

--- JaPedidos/src/main/java/japedidos/bd/BD.java
+++ JaPedidos/src/main/java/japedidos/bd/BD.java
@@ -29,8 +29,8 @@ public final class BD {
 //    public static final String USER = "titanw25_japedidos_hml";
 //    public static final String USER_PWD = "seNai@2024proj";
     
-    public static final String IP = "10.0.0.109";
-    public static final String USER_PWD = "tmb";
+    public static final String IP = "localhost";
+    public static final String USER_PWD = "";
     public static final String USER = "root";
     
     private BD() {}
